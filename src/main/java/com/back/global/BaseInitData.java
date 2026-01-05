781068b0434c1e33cdec59b8048efdf2d447c40f
jin214930 <jin214930@naver.com>
2026-01-05 15:28:40 +0900

    0008

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -1,15 +1,17 @@
 package com.back.global;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class BaseInitData {
     @Bean
     public ApplicationRunner baseInitDataRunner() {
         return args -> {
-            System.out.println("ApplicationRunner 실행");
+            log.debug("ApplicationRunner 실행");
         };
     }
 }
