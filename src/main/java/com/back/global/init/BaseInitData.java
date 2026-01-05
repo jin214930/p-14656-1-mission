0317af1f5c88c60aa522f8322c5e76d63186f7b0
jin214930 <jin214930@naver.com>
2026-01-05 17:02:12 +0900

    0020

--- src/main/java/com/back/global/init/BaseInitData.java
+++ src/main/java/com/back/global/init/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.init;
 
+import com.back.domain.post.comment.service.CommentService;
 import com.back.domain.post.post.document.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class BaseInitData {
     private final PostService postService;
+    private final CommentService commentService;
 
     @Bean
     public ApplicationRunner baseInitDataRunner() {
@@ -22,6 +24,7 @@ public class BaseInitData {
             work3();
             work4();
             work5();
+            work6();
         };
     }
 
@@ -75,5 +78,9 @@ public class BaseInitData {
         }
 
         log.debug("삭제 후 Post 개수: {}", postService.count());
+    }
+
+    private void work6() {
+        log.debug("Comment 개수: {}", commentService.count());
     }
 }
